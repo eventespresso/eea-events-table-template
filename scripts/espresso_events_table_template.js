4531e9ce35f7c57d2fed9567e8c2c0fbbf3c37dc
Brent Christensen <brent@eventespresso.com>
2014-06-02 09:54:33 -0700

    tweaked JS a bit [touch:5980]

--- scripts/espresso_events_table_template.js
+++ scripts/espresso_events_table_template.js
@@ -2,17 +2,18 @@ jQuery(document).ready(function($){
 
 	$("#ee_filter_cat").change(function() {
 		var ee_filter_cat_id = $("option:selected").attr('class');
+		var ee_filter_table_rows = $(".espresso-table-row");
 		console.log(ee_filter_cat_id);
-		$("#ee_filter_table .espresso-table-row").show();
-		$("#ee_filter_table .espresso-table-row").each(function() {
-			if(!$(this).hasClass(ee_filter_cat_id)) {
+		ee_filter_table_rows.each(function() {
+			if ( $(this).hasClass( ee_filter_cat_id ) ) {
+				$(this).show();
+			} else  {
 				$(this).hide();
 			}
 		});
-		if( ee_filter_cat_id == 'ee_filter_show_all') {
-			$("#ee_filter_table .espresso-table-row").show();
+		if( ee_filter_cat_id === 'ee_filter_show_all') {
+			ee_filter_table_rows.show();
 		}
 	});
-
 
 });
