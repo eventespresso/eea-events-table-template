7ce54c0681364deaf98896f62772e4bdcf0d3b20
Seth <seth@eventespresso.com>
2014-06-30 20:00:28 -0600

    Fixing the main_file_path. [touch:5980]

--- EE_Events_Table_Template.class.php
+++ EE_Events_Table_Template.class.php
@@ -33,6 +33,7 @@ Class  EE_Events_Table_Template extends EE_Addon {
 				'version' 					=> EE_EVENTS_TABLE_TEMPLATE_VERSION,
 				'min_core_version' => '4.3.0',
 				'base_path' 				=> EE_EVENTS_TABLE_TEMPLATE_PATH,
+				'main_file_path' => EE_CALENDAR_TABLE_TEMPLATE_PATH . 'espresso-events-table-template.php',
 				'autoloader_paths' => array(
 					'EE_Events_Table_Template' 	=> EE_EVENTS_TABLE_TEMPLATE_PATH . 'EE_Events_Table_Template.class.php',
 				),
